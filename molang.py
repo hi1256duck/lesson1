5619a67d437136b009541c60188d65a9c76f9d51
hi1256duck <anthonykyc11@gmail.com>
2026-03-30 17:12:55 +0100

    CLICKER GAME
    Added messages for win/lose conditions when clicking on characters.

--- molang.py
+++ molang.py
@@ -1,3 +1,5 @@
+from turtle import pos
+
 import pgzrun
 import random
 WIDTH=500
@@ -7,11 +9,14 @@ TITLE="CLICKER GAME"
 Mo=Actor("molang.png")
 score=0
 drums=Actor("drum.png")
+message = ""
 def draw():
     screen.fill(color="blue")
     Mo.draw()
     drums.draw()
     screen.draw.text(str(score),center=(400,50),color="grey")
+    screen.draw.text(message,center=(250,50),color="white")
+    print(message)
 def mo_jump():
     Mo.x=random.randint(0,500)
     Mo.y=random.randint(0,500)
@@ -20,15 +25,22 @@ def drums_jump():
     drums.y=random.randint(0,500)
    
 def  on_mouse_down(pos):
-     global score
+     global score,message
      if Mo.collidepoint(pos):     
          score+=1
+         message="you win!"
          mo_jump()
+
+        
      if drums.collidepoint(pos):
          score-=1
+         message="you lose!"
          drums_jump()
+        
 
-               
+
+
+              
 
 
 
