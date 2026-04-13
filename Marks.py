4703455af29f81ac3ca57ec55bf3eeae4c920901
hi1256duck <anthonykyc11@gmail.com>
2026-04-13 17:16:47 +0100

    marks
    marks people's test randomly.

---
+++ Marks.py
@@ -0,0 +1,15 @@
+import random
+marks=[] 
+for i in range(20):
+    marks.append(random.randint(1,20))
+print(marks)
+
+marks2=[]
+for i in range(30):
+    marks2.append(random.randint(20,50))
+print(marks2)   
+
+marks3=[]
+for i in range(50):
+    marks3.append(random.randint(51,100))
+print(marks3)    
